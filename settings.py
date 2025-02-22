5a54013b83474c3ab145953412b765370dc4c771
Iiya Zakharov <2146376@gmail.com>
2025-02-22 22:53:46 +0300

    Avavava2.1

--- settings.py
+++ settings.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     ALLOWED_USER_IP = os.getenv("ALLOWED_USER_IP")
     GPT_API_KEY = os.getenv("GPT_API_KEY")
     FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
-    REBOOT_HOOK = os.getenv("REBOOT_HOOK")
 
     class Config:
         env_file = '.env'
